1c784ed7ab1568478936a1e574657029303dbc80
edumueller <eduardo.nicastro@icloud.com>
2021-08-28 13:50:51 -0300

    add expiration queueprocess logic

--- src/queues/expiration-queue.ts
+++ src/queues/expiration-queue.ts
@@ -9,3 +9,12 @@ const expirationQueue = new Queue<Payload>('order:expiration', {
     host: process.env.REDIS_HOST,
   },
 });
+
+expirationQueue.process(async (job) => {
+  console.log(
+    'I want to publish an expiration:complete event for oderId',
+    job.data.orderId
+  );
+});
+
+export { expirationQueue };
